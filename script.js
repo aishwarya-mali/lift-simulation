f38e5f444933141308b873009810a9fa91936363
Aishwarya <aishwaryamali29@gmail.com>
2022-08-28 20:32:21 +0530

    [LS] remove comments

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ const btnGenerate = document.querySelector('.btn-generate')
 const floorsInput = document.getElementById('floorsInput')
 const liftsInput = document.getElementById('liftsInput')
 
-let liftPosition = []
-
 btnGenerate.addEventListener('click', function (e) {
-    console.log(`Floors ${+floorsInput.value} Lifts ${+liftsInput.value}`)
     handleFloors()
 })
 
@@ -38,35 +35,23 @@ function handleFloors() {
 }
 
 function handleLifts() {
-    // const floorContainer = document.querySelector('.floor')
-    // console.log(floorContainer)
     let liftsHtml = ''
     for (let i = 0; i <= +liftsInput.value - 1; i++) {
-        // console.log(i, +liftsInput.value)
         liftsHtml += `
             <div class="lift" data-lift=${i + 1}>
                 <div class="lift-left"></div>
                 <div class="lift-right"></div>
             </div>
         `
-        let currentLift = {
-            [`${i + 1}`]: +floorsInput.value
-        }
-        liftPosition.push(currentLift)
     }
     return liftsHtml
 }
 
 document.addEventListener('click', function (e) {
-    let liftEl = document.querySelector(`.lift`)
-    let objIndex = liftPosition.findIndex((obj => obj[liftEl.dataset.lift]));
     if (e.target && e.target.id === 'btn-up') {
-        var { height } = document.querySelector(`[data-floor='${e.target.dataset.floor}']`).getBoundingClientRect()
-        let newPosition = liftPosition[objIndex][liftEl.dataset.lift] = +e.target.dataset.floor
-        liftEl.style.transform = `translateY(-${(height + 80) * (+floorsInput.value - newPosition)}px)`
-        console.log(liftPosition)
+       
     }
     if (e.target && e.target.id === 'btn-down') {
-        liftEl.style.transform = `translateY(${(120 + 80) * ((objIndex + 1) - +e.target.dataset.floor)}px)`
+       
     }
 });
