302be7a1b57d27e6da13f364be2061f76bd98a44
Aishwarya <aishwaryamali29@gmail.com>
2022-09-02 17:50:46 +0530

    [LS] Validation and refactoring

--- script.js
+++ script.js
@@ -4,10 +4,38 @@ const simulationContainer = document.querySelector('.lift-simulation')
 const btnGenerate = document.querySelector('.btn-generate')
 const floorsInput = document.getElementById('floorsInput')
 const liftsInput = document.getElementById('liftsInput')
+const errorFloor = document.querySelector('.errorFloor')
+const errorLift = document.querySelector('.errorLift')
 
 btnGenerate.addEventListener('click', function (e) {
-    handleFloors()
+    handleValidations() && handleFloors()
 })
+
+floorsInput.addEventListener('change', function(){
+    errorFloor.innerHTML = ''
+})
+
+liftsInput.addEventListener('change', function(){
+    errorLift.innerHTML = ''
+})
+
+function handleValidations(){
+    let validationSuccessfull
+    const floorValue = +floorsInput.value
+    const liftValue = +liftsInput.value
+
+    if(!floorValue){
+        errorFloor.innerHTML = 'Please Enter floors'
+        validationSuccessfull = false
+    }
+
+    if(!liftValue){
+        errorLift.innerHTML = 'Please Enter Lifts to Travel'
+        validationSuccessfull = false
+    }
+
+    return validationSuccessfull ?? true
+}
 
 function handleFloors() {
     const floorValue = +floorsInput.value
@@ -16,12 +44,16 @@ function handleFloors() {
             `
             <div class="floor ${i === 0 ? 'lastFloor' : ''}" data-floor=${i}>
                 <div class="lift-buttons">
-                    <button class="btn btn-up" id="btn-up" data-floor=${i}>
-                        &uarr;
-                    </button>
-                    <button class="btn btn-down" id="btn-down" data-floor=${i}>
-                        &darr;
-                    </button>
+                    ${i !== 0 ? (
+                        `<button class="btn btn-up" id="btn-up" data-floor=${i}>
+                            &uarr;
+                        </button>`
+                    ): ''}
+                    ${i!== floorValue-1 ? (
+                        `<button class="btn btn-down" id="btn-down" data-floor=${i}>
+                            &darr;
+                        </button>`
+                    ): ''}
                 </div>
                 ${i === 0 ? `
                 <div class="lifts">
@@ -52,39 +84,12 @@ function handleLifts() {
 document.addEventListener('click', function (e) {
     const floorClicked = +e.target.dataset.floor
     if (e.target && e.target.id === 'btn-up') {
-        const lifts = Array.from(document.querySelectorAll('.lift'))
-        for (let i = 0; i < lifts.length; i++) {
-            const lift = lifts[i]
-            const currentFloor = +lift.dataset.floor
-            const { height } = document.querySelector(`[data-floor='${floorClicked}']`).getBoundingClientRect()
-            if (currentFloor < floorClicked) {
-                lift.style.transform = `translateY(-${(height + 80) * floorClicked}px)`
-                lift.style.transition = `all ${floorClicked}s ease-in`
-                lift.dataset.floor = floorClicked
-                lift.addEventListener('transitionend', function () {
-                    liftDoorAnimation(lift, floorClicked);
-                })
-                break;
-            }
-        }
+        liftsMovement(e.target.id, floorClicked);
     }
     if (e.target && e.target.id === 'btn-down') {
-        const lifts = Array.from(document.querySelectorAll('.lift'))
-        for (let i = 0; i < lifts.length; i++) {
-            const lift = lifts[i]
-            const currentFloor = +lift.dataset.floor
-            const { height } = document.querySelector(`[data-floor='${floorClicked}']`).getBoundingClientRect()
-            if (currentFloor > floorClicked) {
-                lift.style.transform = `translateY(-${(height + 80) * floorClicked}px)`
-                lift.style.transition = `all 3s ease-in`
-                lift.dataset.floor = floorClicked
-                lift.addEventListener('transitionend', function () {
-                    liftDoorAnimation(lift, floorClicked);
-                })
-            }
-            break;
-        }
+        liftsMovement(e.target.id, floorClicked);
     }
+
 });
 
 function liftDoorAnimation(lift, floorClicked) {
@@ -106,4 +111,34 @@ function liftDoorAnimation(lift, floorClicked) {
         leftDoor.classList.remove('closeLeftDoor');
         rightDoor.classList.remove('closeRightDoor');
     }, 3000 * floorClicked);
+}
+
+function liftsMovement(id, floorClicked) {
+    const lifts = Array.from(document.querySelectorAll('.lift'));
+    for (let i = 0; i < lifts.length; i++) {
+        const lift = lifts[i];
+        const currentFloor = +lift.dataset.floor;
+        const { height } = document.querySelector(`[data-floor='${floorClicked}']`).getBoundingClientRect();
+        const floorCondition = id === 'btn-down' ? currentFloor > floorClicked : currentFloor < floorClicked
+        if (floorCondition && currentFloor !== floorClicked) {
+            lift.style.transform = `translateY(-${(height + 80) * floorClicked}px)`;
+            lift.style.transition = `all ${floorClicked + 1}s ease-in`;
+            lift.dataset.floor = floorClicked;
+            enableDisableButtons(true)
+            lift.addEventListener('transitionend', function () {
+                liftDoorAnimation(lift, floorClicked);
+            });
+            setTimeout(() => {
+                enableDisableButtons(false)
+            }, 3000 * floorClicked);
+        }
+        break;
+    }
+}
+
+function enableDisableButtons(value){
+    const buttons = document.querySelectorAll('.btn')
+    buttons.forEach(btn => {
+        btn.disabled = value
+    })
 }
